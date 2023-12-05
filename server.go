d849aca572a76384571689ca2f94167893c047bb
eko <swdev.bali@gmail.com>
2023-12-05 15:31:37 +0700

    sudah ada login dan dashbord, tapi belum ke db

--- server.go
+++ server.go
@@ -4,26 +4,61 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
+
 	// Handle root / default route
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
-
-	r.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("About"))
-	})
-
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/about", AboutHandler)
 	r.HandleFunc("/search", SearchHandler).Methods("GET")
+	r.HandleFunc("/login", LoginPageHandler).Methods("GET")
+	r.HandleFunc("/login", LoginHandler).Methods("POST")
+	r.HandleFunc("/dashboard", DashboardHandler)
 
 	http.Handle("/", r)
-	fmt.Println("Server ready")
-	http.ListenAndServe(":8989", nil)
+	fmt.Println("Server ready 1")
+	http.ListenAndServe(":8080", nil)
+}
 
+func AboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("About"))
+}
+
+func LoginPageHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/login.html")
+}
+
+func DashboardHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/dashboard.html")
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	// Mengelola data masukan pengguna dari form login
+	username := r.PostFormValue("username")
+	password := r.PostFormValue("password")
+
+	// Anda dapat melakukan autentikasi pengguna di sini
+	// Misalnya, memeriksa username dan password dengan data yang valid
+	// Maksudnya, diquery dari basis data pengguna.
+
+	// Jika autentikasi berhasil, Anda dapat mengarahkan pengguna ke halaman lain
+	// atau memberikan respons sukses
+	if username == "admin" && password == "password" {
+		http.Redirect(w, r, "/dashboard", http.StatusFound)
+		return
+	}
+
+	// Jika autentikasi gagal, Anda dapat menampilkan pesan kesalahan
+	errorMessage := "Username atau password salah"
+	http.Error(w, errorMessage, http.StatusUnauthorized)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
 }
 
 func SearchHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,16 +66,16 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	query := vars.Get("q")
 	sA := vars.Get("a")
 	sB := vars.Get("b")
-	
-	a, errA  := strconv.Atoi(sA)
+
+	a, errA := strconv.Atoi(sA)
 	b, errB := strconv.Atoi(sB)
 
 	if errA != nil || errB != nil {
-        http.Error(w, "Parameter a dan b harus berupa bilangan", http.StatusBadRequest)
-        return
-    }
+		http.Error(w, "Parameter a dan b harus berupa bilangan", http.StatusBadRequest)
+		return
+	}
 
 	c := a + b
-	responseMessage := fmt.Sprintf("Hasil pencarian untuk: %s. Penjumlahan: %d+%d=%d", query,a,b,c)
+	responseMessage := fmt.Sprintf("Hasil pencarian untuk: %s. Penjumlahan: %d+%d=%d", query, a, b, c)
 	w.Write([]byte(responseMessage))
 }
